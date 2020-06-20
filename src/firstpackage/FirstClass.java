b43af9b99acc15ba31ecc597bfe6530a3874f00d
eypozdemir <67050606+eypozdemir@users.noreply.github.com>
2020-06-20 22:16:07 +0300

    Update FirstClass.java
    ilk commit from remote repository

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,8 +3,10 @@ package firstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-	System.out.println("First Commit from local repository");
-
+		System.out.println("First Commit from local repository");
+		System.out.println("second commit from local repository");
+		System.out.println("third commit from local repository");
+		System.out.println("First Commit from remote repository");
 	}
 
 }
