72e589c2ba84a019f70fbc586a658122ad636069
Eyyup <eyp070913@gmail.com>
2020-06-20 22:41:54 +0300

    son conflict bugun icin

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -9,7 +9,7 @@ public class FirstClass {
 
 		
 
-		System.out.println("conflict yapmak istiyoruz");
+		System.out.println("2. kez conflict yapiyoruz");
 
 		System.out.println("First Commit from remote repository");
 
