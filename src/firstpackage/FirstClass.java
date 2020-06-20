1250f3741e3d386bb3dcf401eff7bd070537b69c
Eyyup <eyp070913@gmail.com>
2020-06-20 21:35:09 +0300

    ikinci commit local repositoryden

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -3,7 +3,8 @@ package firstpackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-	System.out.println("First Commit");
+	System.out.println("First commit from local repository");
+	System.out.println("second commit from local repository");
 
 	}
 
